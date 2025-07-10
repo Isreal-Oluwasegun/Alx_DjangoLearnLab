e54b37cfe731fed0cebdf7dc0c0bd9c61a0f082a
Israel Oluwasegun <marksegman@outlook.com>
2025-07-10 01:27:56 +0100

    inital modify admin

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ["title","author", "publication_year"]
+    list_display = ("title","author", "publication_year")
     list_filter = "publication_year"
     search_fields = "author"
-    
+
 admin.site.register(Book)
