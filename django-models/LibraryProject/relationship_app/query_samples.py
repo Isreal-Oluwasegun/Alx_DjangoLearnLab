eda7bee8363009c9f8aebfa4da83ba20244c4315
Israel Oluwasegun <marksegman@outlook.com>
2025-07-18 06:01:10 +0100

    modify query file

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -7,5 +7,6 @@ library_name = "Obj library"
 library = Library.objects.get(name=library_name)
 books_in_library = library.books.all()
 
-Library.objects.get(name=library_name).books.all()
-Librarian.objects.filter(library__name__iexact="obj library")
+library_name = "Obj library"
+library = Library.objects.get(name=library_name)
+librarian = Librarian.objects.get(library=library)
