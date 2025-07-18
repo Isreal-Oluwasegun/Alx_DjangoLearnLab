557b33e769d42ec62a0c9c644577cb23e336d8c1
Israel Oluwasegun <marksegman@outlook.com>
2025-07-18 14:34:54 +0100

    add task1

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,7 +5,7 @@ from .models import Library
 from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-
+from django.contrib.auth import login
 
 def list_books(request):
     books = Book.objects.all()
@@ -21,4 +21,9 @@ class RegistrationView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'relationship_app/register.html'
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        login(self.request, self.object) 
+        return response
 
