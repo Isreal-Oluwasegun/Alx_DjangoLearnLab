fa7c1042b46495ed6421a0e7365c09f1b753413d
Israel Oluwasegun <marksegman@outlook.com>
2025-07-18 05:58:32 +0100

    modify query file

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,4 +1,11 @@
-Book.objects.filter(name="Mk")
+author_name = "Israel"
+author = Author.objects.get(name=author_name)
+books_by_author = Book.objects.filter(author=author)
+
+
 library_name = "Obj library"
+library = Library.objects.get(name=library_name)
+books_in_library = library.books.all()
+
 Library.objects.get(name=library_name).books.all()
 Librarian.objects.filter(library__name__iexact="obj library")
