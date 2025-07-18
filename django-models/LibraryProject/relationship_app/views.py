ec072e0c7f0ccfceb64d2ab35c933e5c9e9d57d2
Israel Oluwasegun <marksegman@outlook.com>
2025-07-18 08:45:01 +0100

    Fix line ending warnings

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from relationship_app.models import Book, Author, Library, Librarian
 
 def all_books(request):
-    books = Book.objects.select_related('author').all()
+    books = Book.objects.all()
     return render(request, 'relationship_app/library_detail.html', {'books':books})
