3f61a52d4dc065562559e1e061235a30d61fac98
Israel Oluwasegun <marksegman@outlook.com>
2025-07-30 22:12:10 +0100

    modify custom manager

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
 from .models import Book
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+from .models import CustomUser
+
+
+
+
+class UserAdmin(DefaultUserAdmin):
+    model = CustomUser
+    fieldsets = (
+        DefaultUserAdmin.fieldsets + (
+            (None, {'fields': ('date_of_birth', 'profile_photo')})
+        )
+
+    )
+    add_fieldsets = (
+        DefaultUserAdmin.add_fieldsets + (None, {'fields':('date_of_birth')})
+    )
+
+
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title','author', 'publication_year')
@@ -7,3 +26,4 @@ class BookAdmin(admin.ModelAdmin):
     search_fields = ("author",)
 
 admin.site.register(Book, BookAdmin)
+admin.site.register(CustomUser, UserAdmin)
--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -1,18 +1,3 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
 from .models import User
-
-class UserAdmin(DefaultUserAdmin):
-    model = User
-    fieldsets = (
-        DefaultUserAdmin.fieldsets + (
-            (None, {'fields': ('date_of_birth', 'profile_photo')})
-        )
-
-    )
-    add_fieldsets = (
-        DefaultUserAdmin.add_fieldsets + (None, {'fields':('date_of_birth')})
-    )
-
-
-admin.site.register(User, UserAdmin)
